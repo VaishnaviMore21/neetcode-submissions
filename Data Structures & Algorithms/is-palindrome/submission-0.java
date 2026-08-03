c2a5abcfc49a7d385037e46bbb47e4002b24f1d3
Vaishnavi More <143414333+VaishnaviMore21@users.noreply.github.com>
2026-08-03 11:37:24 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            // Move left pointer until it points to an alphanumeric character
+            while (left < right && !isAlphanumeric(s.charAt(left))) {
+                left++;
+            }
+            // Move right pointer until it points to an alphanumeric character
+            while (right > left && !isAlphanumeric(s.charAt(right))) {
+                right--;
+            }
+
+            // Compare characters case-insensitively
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    private boolean isAlphanumeric(char c) {
+        return (c >= 'A' && c <= 'Z') || 
+               (c >= 'a' && c <= 'z') || 
+               (c >= '0' && c <= '9');
+    }
+}
